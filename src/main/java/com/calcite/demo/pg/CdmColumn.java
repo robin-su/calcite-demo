857a9e9df01abfbcaacc40d0eb855c24887e2aa7
robin-su <549570881@qq.com>
2023-03-11 16:05:50 +0800

    Add postgresql datasource

---
+++ src/main/java/com/calcite/demo/pg/CdmColumn.java
@@ -0,0 +1,11 @@
+package com.calcite.demo.pg;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CdmColun {
+}
---
+++ src/main/java/com/calcite/demo/pg/PostgreSqlTableScan.java
@@ -0,0 +1,2 @@
+package com.calcite.demo.pg;public class PostgreSqlTableScan {
+}
